b09b4ce53d7d5ba9dcabd5cd5958a183d763ac8a
emmmmilyim <emmmmilyim@gmail.com>
2020-06-04 00:27:29 -0700

    Edit #2

--- js/scripts.js
+++ js/scripts.js
@@ -4,17 +4,8 @@ const thumbBar = document.querySelector('.thumb-bar');
 const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
-/* Looping through images */
-
-for(let i = 1; i <= 5; i++) {
-  const newImage = document.createElement('img');
-  newImage.setAttribute('src', 'images/pic' + i + '.jpg');
-  thumbBar.appendChild(newImage);
-
-
-    newImage.onclick = function(showImage) {
-  displayedImage.src = showImage.target.src;
-  var newImgPath = (showImage.target.src).toString();
-  var arrLoc = newImgPath.charAt(newImgPath.length-5);
-  }
-}
+$(document).ready(function() {
+  $("#button").click(function() {
+    displayPics();
+  });
+});
